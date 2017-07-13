a903a518615574be70eb55a8e22134bcce9dc937
Aaron Sosa <aarontsosa@gmail.com>
2017-07-13 15:39:40 -0400

    Trying to add bootstrap tabs

---
+++ AnimalCraft.js
@@ -0,0 +1,5 @@
+
+$('#myTabs a[href="#tiger"]').tab('show') // Select tab by name
+$('#myTabs a[href="#panda"]').tab('show') // Select tab by name
+$('#myTabs a[href="#penguin"]').tab('show') // Select tab by name
+$('#myTabs a[href="#monkey"]').tab('show') // Select tab by name
