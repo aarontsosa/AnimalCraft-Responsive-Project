b5a813cda5460319c0589a5b0f57c6b2cdcab8ea
Aaron Sosa <aarontsosa@gmail.com>
2017-07-14 10:37:35 -0400

    on hold till later

--- AnimalCraft.js
+++ AnimalCraft.js
@@ -1,5 +1,37 @@
 
-$('#myTabs a[href="#tiger-tab"]').tab('show') // Select tab by name
-$('#myTabs a[href="#panda-tab"]').tab('show') // Select tab by name
-$('#myTabs a[href="#penguin-tab"]').tab('show') // Select tab by name
-$('#myTabs a[href="#monkey-tab"]').tab('show') // Select tab by name
+// $('a[href="#tiger-tab"]').tab('show') // Select tab by name
+// $('a[href="#panda-tab"]').tab('show') // Select tab by name
+// $('a[href="#penguin-tab"]').tab('show') // Select tab by name
+// $('a[href="#monkey-tab"]').tab('show') // Select tab by name
+
+$("a.tiger-tab").on('click',function(){
+    $(".tab-pane").hide();
+    $($(this).attr("href")).show();
+  });
+
+$("a.monkey-tab").on('click',function(){
+    $(".tab-pane").hide();
+    $($(this).attr("href")).show();
+  });
+
+$("a.panda-tab").on('click',function(){
+    $(".tab-pane").hide();
+    $($(this).attr("href")).show();
+  });
+
+$("a.penguin-tab").on('click',function(){
+    $(".tab-pane").hide();
+    $($(this).attr("href")).show();
+  });
+
+
+
+// $('#myTabs a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
+
+// $('#myTabs2 a').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
